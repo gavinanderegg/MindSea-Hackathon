ed9fc477da3ca97f7d3c94dc76637d2f5a96bb4a
Gavin Anderegg <gavin@anderegg.ca>
2013-11-13 21:40:50 -0400

    Routing is more snazzy

--- wiki/js/main.js
+++ wiki/js/main.js
@@ -1,20 +1,14 @@
 var wikiModule = angular.module('wiki', ['ngRoute']);
 
 wikiModule.config(function($locationProvider, $routeProvider) {
-	$locationProvider.html5Mode(false);
+	$locationProvider.html5Mode(false); // For ease of demo-ing
+		
+	$routeProvider.when('/:id?', {
+		controller: 'wikiController',
+		templateUrl: 'content.html'
+	});
 });
 
-wikiModule.controller('wikiController', function($scope, $location) {
-	$scope.loc = $location; // this is lame - switch to routes
-	
-	$scope.go = function(path) {
-		$location.path('/newValue');
-	};
-	
-	$scope.$watch('loc.path()', function() {
-		console.log('called!');
-		
-		var title = $location.path();
-		$scope.title = title;
-	});
+wikiModule.controller('wikiController', function($scope, $routeParams) {
+	console.log($routeParams);
 });
---
+++ wiki/notes.md
@@ -0,0 +1,5 @@
+
+- cache.manifest - http://diveintohtml5.info/offline.html
+- localstorage
+- Some responsive CSS framework
+- 
