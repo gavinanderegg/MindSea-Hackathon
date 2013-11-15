ca283c3c6e048429a01cf3539d34a10c82b2315c
Gavin Anderegg <gavin@anderegg.ca>
2013-11-14 21:58:53 -0400

    Wiki!

--- wiki/js/main.js
+++ wiki/js/main.js
@@ -1,8 +1,9 @@
 var wikiModule = angular.module('wiki', ['ngRoute']);
 
-wikiModule.config(function($locationProvider, $routeProvider) {
+wikiModule.config(function($locationProvider, $routeProvider, $sceProvider) {
 	$locationProvider.html5Mode(false); // For ease of demo-ing
-		
+	$sceProvider.enabled(false); // because we're doing crazy things
+	
 	$routeProvider.when('/:slug?', {
 		controller: 'contentController',
 		templateUrl: 'content.html'
@@ -21,20 +22,23 @@ wikiModule.factory('Page', function() {
 wikiModule.controller('wikiController', function($scope, Page) {
 	$scope.Page = Page;
 	
-	$scope.data = {
-		'Main_Page': 'Welcome to the main page!',
-		'foo': 'the foo page',
-	};
+	if (localStorage.getItem('data') !== null) {
+		$scope.data = JSON.parse(localStorage.getItem('data'));
+	} else {
+		$scope.data = {
+			'Main_Page': 'Welcome to the <a href="/#/foo">main</a> page!',
+			'foo': 'the foo page'
+		};
+	}
 });
 
 wikiModule.controller('contentController', function($scope, $routeParams, Page) {
-	// Check to see if the slug has content
-	// if it doesn't exist, make it in $scope.content
-	
 	var slug = $routeParams.slug;
-	// var titleToSlug = function(title) {
-	//	return title.replace(/\s+/g, '_').replace(/[~`!@#$%\^&*()-+={}\[\]\.,\\\/\?"':;|]/g, '');
-	// };
+	
+	var titleToSlug = function(title) {
+		return title.replace(/\s+/g, '_').replace(/[~`!@#$%\^&*()-+={}\[\]\.,\\\/\?"':;|]/g, '');
+	};
+	
 	var slugToTitle = function(slug) {
 		return slug.replace(/_/g, ' ');
 	};
@@ -45,6 +49,21 @@ wikiModule.controller('contentController', function($scope, $routeParams, Page) 
 	} else {
 		Page.setTitle(slugToTitle(slug));
 	}
+	
+	$scope.updateData = function() {
+		$scope.data[slug] = angular.element(document.querySelector('#content')).html();
+	};
+	
+	$scope.link = function() {
+		var link = document.createElement('a');
+		link.href = '/#/' + titleToSlug(window.getSelection().toString());
+		window.getSelection().getRangeAt(0).surroundContents(link);
+		$scope.updateData();
+	};
+	
+	$scope.save = function() {
+		localStorage.setItem('data', JSON.stringify($scope.data));
+	};
 	
 	$scope.content = $scope.data[slug];
 });
